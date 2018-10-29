8eb88d60738d73617803bff624bc506aa7bfdd92
Ayax Alexis Casarrubias Rodríguez <casarrubias2837@gmail.com>
2018-10-29 16:01:00 -0600

    Add files via upload

---
+++ Simulador/Assets/Scripts/Fondo.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Fondo : MonoBehaviour {
+
+    public Dropdown m_Dropdown;
+    public Material Skybox1;
+    public Material Skybox2;
+    public Material Skybox3;
+    // Use this for initialization
+    public void Dropdown_IndexChange(int index){
+        if (index==0){
+            RenderSettings.skybox = Skybox1;
+        }
+        if (index == 1)
+        {
+            RenderSettings.skybox = Skybox2;
+        }
+        if (index == 2)
+        {
+            RenderSettings.skybox = Skybox3;
+        }
+
+    }
+
+    void Start () {
+        RenderSettings.skybox = Skybox1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		
+	}
+}
---
+++ Simulador/Assets/Scripts/switchCamera.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class switchCamera : MonoBehaviour
+{
+
+    public Camera cam1;
+    public Camera cam2;
+    public Camera cam3;
+
+    private float timer = 0;
+    void Start()
+    {
+        cam1.enabled = true;
+        cam2.enabled = false;
+        cam3.enabled = false;
+
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > 10 && timer < 20)
+        {
+            cam1.enabled = false;
+            cam2.enabled = true;
+            cam3.enabled = false;
+
+        }
+        else
+        {
+            if (timer > 20 && timer < 30)
+            {
+                cam1.enabled = false;
+                cam2.enabled = false;
+                cam3.enabled = true;
+            }
+            if (timer > 30) { 
+
+                timer = 0;
+            }
+        }
+        
+    }
+}
